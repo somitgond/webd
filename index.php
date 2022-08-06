e17ba2e82c39e03be517b841b8677dcb3afde341
Somit Gond <62469008+somitgond@users.noreply.github.com>
2022-08-06 18:40:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,21 @@
     <title>Rupees to Dollar convertor</title>
 </head>
 <body>
+    <?php
+    if(isset($_POST['rup'])){
+        $doll=80*$_POST['rup'];
+    }else{
+        $doll=1;
+    }
+    ?>
     <h1> Welcome to our website </h1>
     <p> You can convert your rupees to dollar using our service </p>
 <form method="post" action='index.php' >
     <input type="number" name="rup" placeholder="Enter rupees">
     <input type="submit" >
 </form>
-
+<?php 
+    echo "<p>Your amount in Dollar is $doll<p><br>";
+?>
 </body>
 </html>
